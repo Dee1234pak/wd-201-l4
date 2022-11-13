69db969dfff08b5d9ad400f276c808ee8364bcc2
Dee1234pak <89565631+Dee1234pak@users.noreply.github.com>
2022-11-13 13:40:41 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,29 +1,70 @@
-const todoList = required("../todo");
+const todoList = () => {
+  const formattedDate = (d) => {
+    return d.toISOString().split("T")[0];
+  };
+  const dateToday = new Date();
+  const today = formattedDate(dateToday);
+  const yesterday = formattedDate(
+    new Date(new Date().setDate(dateToday.getDate() - 1))
+  );
+  const tomorrow = formattedDate(
+    new Date(new Date().setDate(dateToday.getDate() + 1))
+  );
 
-const { all , markAsComplete, add } = todoList();
+  let all = [];
+  const add = (todoItem) => {
+    all.push(todoItem);
+  };
+  const markAsComplete = (index) => {
+    all[index].completed = true;
+  };
 
-describe("Todolist Test Suite", () => {
-    beforeAll(() => {
-        add({
-            title: "new todo",
-            complete: false,
-            dueDate: new Date().toLocaleDateString("en-CA"),
-        });
+  const overdue = () => {
+    return all.filter((item) => {
+      return item.dueDate <= yesterday && item.completed === false;
     });
+  };
 
-    TextDecoderStream("Should add new todo", () => {
-        const todoItemsCount = all.length;
-        add({
-            title: "Test todo",
-            completed: "false",
-            dueDate: new Date().toLocaleDateString("en-CA"),
-        })
-        except(all.length).toBe(todoItemsCount + 1);
-    })
-
-    Test("should mark as todo as complete0", () => {
-        expect(all[0].completed).toBe(false);
-        marksAsComplete(0);
-        expect(all[0].completed).toBe(true);
+  const dueToday = () => {
+    return all.filter((item) => {
+      return item.dueDate === today;
     });
-});
+  };
+
+  const dueLater = () => {
+    return all.filter((item) => {
+      return item.dueDate >= tomorrow;
+    });
+  };
+
+  // eslint-disable-next-line no-unused-vars
+  const toDisplayableList = (list) => {
+    let mylist = [];
+    list.forEach((item) => {
+      if (item.dueDate === today) {
+        if (item.completed === true) {
+          mylist.push(`[x] ${item.title}`);
+        } else {
+          mylist.push(`[ ] ${item.title}`);
+        }
+      } else {
+        if (item.completed === true) {
+          mylist.push(`[x] ${item.title} ${item.dueDate}`);
+        } else {
+          mylist.push(`[ ] ${item.title} ${item.dueDate}`);
+        }
+      }
+    });
+    return mylist.join("\n");
+  };
+   return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+  };
+};
+
+module.exports = todoList;
